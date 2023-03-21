92710bec9ca325984bd3d579a7517f298b234714
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-03-21 14:38:04 +0900

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Seungchul Ha on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
